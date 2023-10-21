e7f57d487b06546f5cfbd257e5ed3510ab911db2
Breeze <echobydq@gmail.com>
2023-10-21 15:32:21 +0800

    debug

--- debug/moc_login.cpp
+++ debug/moc_login.cpp
@@ -8,6 +8,7 @@
 
 #include <memory>
 #include "../login.h"
+#include <QtGui/qtextcursor.h>
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
@@ -22,8 +23,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_login_t {
-    const uint offsetsAndSize[12];
-    char stringdata0[88];
+    const uint offsetsAndSize[10];
+    char stringdata0[65];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(offsetof(qt_meta_stringdata_login_t, stringdata0) + ofs), len 
@@ -33,12 +34,11 @@ QT_MOC_LITERAL(0, 5), // "login"
 QT_MOC_LITERAL(6, 17), // "on_btnMin_clicked"
 QT_MOC_LITERAL(24, 0), // ""
 QT_MOC_LITERAL(25, 19), // "on_btnClose_clicked"
-QT_MOC_LITERAL(45, 22), // "on_btnRegister_clicked"
-QT_MOC_LITERAL(68, 19) // "on_btnLogin_clicked"
+QT_MOC_LITERAL(45, 19) // "on_btnLogin_clicked"
 
     },
     "login\0on_btnMin_clicked\0\0on_btnClose_clicked\0"
-    "on_btnRegister_clicked\0on_btnLogin_clicked"
+    "on_btnLogin_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -48,7 +48,7 @@ static const uint qt_meta_data_login[] = {
       10,       // revision
        0,       // classname
        0,    0, // classinfo
-       4,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -56,13 +56,11 @@ static const uint qt_meta_data_login[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   38,    2, 0x08,    1 /* Private */,
-       3,    0,   39,    2, 0x08,    2 /* Private */,
-       4,    0,   40,    2, 0x08,    3 /* Private */,
-       5,    0,   41,    2, 0x08,    4 /* Private */,
+       1,    0,   32,    2, 0x08,    1 /* Private */,
+       3,    0,   33,    2, 0x08,    2 /* Private */,
+       4,    0,   34,    2, 0x08,    3 /* Private */,
 
  // slots: parameters
-    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -78,8 +76,7 @@ void login::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void 
         switch (_id) {
         case 0: _t->on_btnMin_clicked(); break;
         case 1: _t->on_btnClose_clicked(); break;
-        case 2: _t->on_btnRegister_clicked(); break;
-        case 3: _t->on_btnLogin_clicked(); break;
+        case 2: _t->on_btnLogin_clicked(); break;
         default: ;
         }
     }
@@ -94,7 +91,7 @@ const QMetaObject login::staticMetaObject = { {
     nullptr,
 qt_incomplete_metaTypeArray<qt_meta_stringdata_login_t
 , QtPrivate::TypeAndForceComplete<login, std::true_type>
-, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
 
 
 >,
@@ -121,13 +118,13 @@ int login::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 4)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 4;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 4)
+        if (_id < 3)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 4;
+        _id -= 3;
     }
     return _id;
 }
