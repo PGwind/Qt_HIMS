f756bc2098f9cc2b7fcfd8feab761a1df2f45e2a
Breeze <echobydq@gmail.com>
2023-10-22 22:59:53 +0800

    user

--- userwindow.cpp
+++ userwindow.cpp
@@ -42,9 +42,121 @@ userwindow::userwindow(QWidget *parent, const QString &loginCount) :
     ui->toolBarUser->addWidget(widget);
 
     qDebug() << loginCount;
+
+    // 性别
+    ui->comboBox_sex->addItem("男");
+    ui->comboBox_sex->addItem("女");
+    // 楼号
+    ui->comboBox_build->addItem("1");
+    ui->comboBox_build->addItem("2");
+    ui->comboBox_build->addItem("3");
+    ui->comboBox_build->addItem("4");
+    // 床号
+    ui->comboBox_bed->addItem("1");
+    ui->comboBox_bed->addItem("2");
+    ui->comboBox_bed->addItem("3");
+    // 科室
+    QStringList items;
+    items << "呼吸内科" << "消化内科" << "泌尿内科" << "心内科" << "血液科" << "内分泌科"
+          << "神经内科" << "小儿科" << "感染科" << "普外科" << "骨科" << "神经外科"
+          << "肝胆外科" << "泌尿外科" << "烧伤科" << "妇科" << "产科" << "血透室" << "重症监护室";
+    items.sort();
+    ui->comboBox_department->addItems(items);
 }
 
 userwindow::~userwindow()
 {
     delete ui;
 }
+
+void userwindow::updateRecord(QSqlRecord &recData)
+{
+    m_record= recData;
+    ui->lineEdit_id->setEnabled(false);
+    setWindowTitle("更新记录");
+
+    ui->lineEdit_id->setText(recData.value("id").toString());
+    ui->lineEdit_name->setText(recData.value("name").toString());
+    ui->spinBox_age->setValue(recData.value("age").toInt());
+    ui->comboBox_sex->setEditText(recData.value("gender").toString());
+    ui->lineEdit_idNumber->setText(recData.value("idNumber").toString());
+    ui->lineEdit_address->setText(recData.value("address").toString());
+
+    QVariant va= recData.value("photo");
+    if (!va.isValid()) //Photo 字段内容为空
+        ui->label_photo->clear();
+    else
+    {
+        QByteArray data= va.toByteArray();
+        QPixmap pic;
+//        pic.loadFromData(data);
+//        ui->label_photo->setPixmap(pic.scaledToWidth(ui->label_photo->size().width()));
+        if (pic.loadFromData(data))
+        {
+            ui->label_photo->setPixmap(pic.scaledToWidth(ui->label_photo->size().width()));
+        }
+        else
+        {
+            qDebug() << "加载图像失败";
+        }
+    }
+
+}
+
+void userwindow::addRecord(QSqlRecord &recData)
+{
+    m_record = recData;
+    ui->lineEdit_id->setEnabled(false);
+    setWindowTitle("插入新记录");
+    ui->lineEdit_id->setText(recData.value("id").toString());
+
+}
+
+QSqlRecord userwindow::getRecordData()
+{
+    m_record.setValue("id", ui->lineEdit_id->text().toInt());
+    m_record.setValue("name", ui->lineEdit_name->text());
+    m_record.setValue("age", ui->spinBox_age->value());
+    m_record.setValue("gender", ui->comboBox_sex->currentText());
+    m_record.setValue("idNumber", ui->lineEdit_name->text());
+    m_record.setValue("address", ui->lineEdit_name->text());
+
+    return m_record;
+}
+
+void userwindow::on_actClear_triggered()
+{
+    ui->label_photo->clear();
+    m_record.setNull("photo");
+}
+
+
+void userwindow::on_actPhoto_triggered()
+{
+    QString aFile= QFileDialog::getOpenFileName(this,"选择图片文件","", "照片(*.jpg)");
+    if (aFile.isEmpty())
+        return;
+    QByteArray data;
+    QFile* file= new QFile(aFile);
+    file->open(QIODevice::ReadOnly);
+    data = file->readAll();
+    file->close();
+    delete file;
+    m_record.setValue("photo",data); //图片保存到 Photo 字段中
+    QPixmap pic;
+    pic.loadFromData(data);
+    ui->label_photo->setPixmap(pic.scaledToWidth(ui->label_photo->size().width()));
+}
+
+
+void userwindow::on_actSave_triggered()
+{
+    isSave = true;
+}
+
+
+void userwindow::on_actCancel_triggered()
+{
+    this->close();
+}
+
--- userwindow.h
+++ userwindow.h
@@ -7,6 +7,8 @@
 #include <QSpacerItem>
 #include <QHBoxLayout>
 #include <QFile>
+#include <QtSql>
+#include <QFileDialog>
 
 namespace Ui {
 class userwindow;
@@ -15,10 +17,25 @@ class userwindow;
 class userwindow : public QMainWindow
 {
     Q_OBJECT
+private:
+    QSqlRecord m_record;
 
 public:
+    bool isSave = false;
     explicit userwindow(QWidget *parent = nullptr, const QString &loginCount = nullptr);
     ~userwindow();
+    void updateRecord(QSqlRecord &recData); //更新记录
+    void addRecord(QSqlRecord &recData); //插入记录
+    QSqlRecord getRecordData(); //获取界面中输入的数据
+
+private slots:
+    void on_actClear_triggered();
+
+    void on_actPhoto_triggered();
+
+    void on_actSave_triggered();
+
+    void on_actCancel_triggered();
 
 private:
     Ui::userwindow *ui;
