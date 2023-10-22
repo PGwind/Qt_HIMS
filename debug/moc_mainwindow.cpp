38ff930dee18719b2d0cdd5ff53955bf231f1d86
Breeze <echobydq@gmail.com>
2023-10-22 23:00:07 +0800

    debug

--- debug/moc_mainwindow.cpp
+++ debug/moc_mainwindow.cpp
@@ -8,6 +8,7 @@
 
 #include <memory>
 #include "../mainwindow.h"
+#include <QtGui/qtextcursor.h>
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
@@ -22,17 +23,27 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    const uint offsetsAndSize[2];
-    char stringdata0[11];
+    const uint offsetsAndSize[16];
+    char stringdata0[123];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs), len 
 static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
     {
-QT_MOC_LITERAL(0, 10) // "MainWindow"
+QT_MOC_LITERAL(0, 10), // "MainWindow"
+QT_MOC_LITERAL(11, 22), // "on_actModify_triggered"
+QT_MOC_LITERAL(34, 0), // ""
+QT_MOC_LITERAL(35, 26), // "on_tableView_doubleClicked"
+QT_MOC_LITERAL(62, 11), // "QModelIndex"
+QT_MOC_LITERAL(74, 5), // "index"
+QT_MOC_LITERAL(80, 19), // "on_actAdd_triggered"
+QT_MOC_LITERAL(100, 22) // "on_actDelete_triggered"
 
     },
-    "MainWindow"
+    "MainWindow\0on_actModify_triggered\0\0"
+    "on_tableView_doubleClicked\0QModelIndex\0"
+    "index\0on_actAdd_triggered\0"
+    "on_actDelete_triggered"
 };
 #undef QT_MOC_LITERAL
 
@@ -42,22 +53,41 @@ static const uint qt_meta_data_MainWindow[] = {
       10,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   38,    2, 0x08,    1 /* Private */,
+       3,    1,   39,    2, 0x08,    2 /* Private */,
+       6,    0,   42,    2, 0x08,    4 /* Private */,
+       7,    0,   43,    2, 0x08,    5 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 4,    5,
+    QMetaType::Void,
+    QMetaType::Void,
+
        0        // eod
 };
 
 void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    (void)_o;
-    (void)_id;
-    (void)_c;
-    (void)_a;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_actModify_triggered(); break;
+        case 1: _t->on_tableView_doubleClicked((*reinterpret_cast< std::add_pointer_t<QModelIndex>>(_a[1]))); break;
+        case 2: _t->on_actAdd_triggered(); break;
+        case 3: _t->on_actDelete_triggered(); break;
+        default: ;
+        }
+    }
 }
 
 const QMetaObject MainWindow::staticMetaObject = { {
@@ -68,7 +98,7 @@ const QMetaObject MainWindow::staticMetaObject = { {
     nullptr,
 qt_incomplete_metaTypeArray<qt_meta_stringdata_MainWindow_t
 , QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>
-
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<const QModelIndex &, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
 
 
 >,
@@ -92,6 +122,17 @@ void *MainWindow::qt_metacast(const char *_clname)
 int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 4)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 4;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 4)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 4;
+    }
     return _id;
 }
 QT_WARNING_POP
--- debug/moc_userwindow.cpp
+++ debug/moc_userwindow.cpp
@@ -22,17 +22,24 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_userwindow_t {
-    const uint offsetsAndSize[2];
-    char stringdata0[11];
+    const uint offsetsAndSize[12];
+    char stringdata0[100];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(offsetof(qt_meta_stringdata_userwindow_t, stringdata0) + ofs), len 
 static const qt_meta_stringdata_userwindow_t qt_meta_stringdata_userwindow = {
     {
-QT_MOC_LITERAL(0, 10) // "userwindow"
+QT_MOC_LITERAL(0, 10), // "userwindow"
+QT_MOC_LITERAL(11, 21), // "on_actClear_triggered"
+QT_MOC_LITERAL(33, 0), // ""
+QT_MOC_LITERAL(34, 21), // "on_actPhoto_triggered"
+QT_MOC_LITERAL(56, 20), // "on_actSave_triggered"
+QT_MOC_LITERAL(77, 22) // "on_actCancel_triggered"
 
     },
-    "userwindow"
+    "userwindow\0on_actClear_triggered\0\0"
+    "on_actPhoto_triggered\0on_actSave_triggered\0"
+    "on_actCancel_triggered"
 };
 #undef QT_MOC_LITERAL
 
@@ -42,21 +49,41 @@ static const uint qt_meta_data_userwindow[] = {
       10,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   38,    2, 0x08,    1 /* Private */,
+       3,    0,   39,    2, 0x08,    2 /* Private */,
+       4,    0,   40,    2, 0x08,    3 /* Private */,
+       5,    0,   41,    2, 0x08,    4 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
        0        // eod
 };
 
 void userwindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    (void)_o;
-    (void)_id;
-    (void)_c;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<userwindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_actClear_triggered(); break;
+        case 1: _t->on_actPhoto_triggered(); break;
+        case 2: _t->on_actSave_triggered(); break;
+        case 3: _t->on_actCancel_triggered(); break;
+        default: ;
+        }
+    }
     (void)_a;
 }
 
@@ -68,7 +95,7 @@ const QMetaObject userwindow::staticMetaObject = { {
     nullptr,
 qt_incomplete_metaTypeArray<qt_meta_stringdata_userwindow_t
 , QtPrivate::TypeAndForceComplete<userwindow, std::true_type>
-
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
 
 
 >,
@@ -92,6 +119,17 @@ void *userwindow::qt_metacast(const char *_clname)
 int userwindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 4)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 4;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 4)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 4;
+    }
     return _id;
 }
 QT_WARNING_POP
