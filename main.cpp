27386315289ddd21cc9018024f60a2a5ce2d342c
Breeze <echobydq@gmail.com>
2023-10-26 23:55:08 +0800

    main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include "mainwindow.h"
 #include <QApplication>
 #include "login.h"
 
@@ -7,14 +6,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     login n;
     n.show();
-
-//    bool flagValue = n.getFlag();
-//    MainWindow w;
-//    w.show();
-//    // 如果 flag 为 true，则打开 MainWindow
-//    if (flagValue) {
-//        w.close();
-//    }
 
     return a.exec();
 }
